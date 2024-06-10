39fea85aae52d6a710525c11821ae873ca3d6737
Martijn <oldmartijntje@gmail.com>
2024-06-10 21:58:11 +0200

    test with user secrets

--- server.js
+++ server.js
@@ -3,11 +3,29 @@ const express = require("express");
 const { connect, users, sessionTokens } = require("./database.js");
 const { exit } = require("process");
 const settings = require("./settings");
-const env = require("./env");
+require('dotenv').config();
+
 const { loginRouter } = require("./authentication/login.routes.js");
 
-const MONGO_URI = env["MONGO_URI"];
+const MONGO_URI = process.env.DB_URL;
 const port = settings["apiPort"];
+
+var visit = 0;
+
+const testRouter = express.Router();
+testRouter.use(express.json());
+
+testRouter.get("/isOnline", async (_req, res) => {
+    visit++;
+    console.log("Connection go brrr...")
+    res.status(200).send({ "message": "The server is online." });
+});
+
+testRouter.get("/visits", async (_req, res) => {
+    visit++;
+    console.log("We have had " + visit + " visits since startup!")
+    res.status(200).send({ "message": "We have had " + visit + " visits since startup!" });
+});
 
 if (!MONGO_URI) {
     console.error("No MONGO_URI environment variable has been defined");
@@ -20,6 +38,7 @@ connect(MONGO_URI)
         const app = express();
         app.use(cors());
         app.use("/login", loginRouter);
+        app.use("/test", testRouter);
         // start the Express server
         app.listen(port, () => {
             console.log(`Server running at http://localhost:${port}...`);
