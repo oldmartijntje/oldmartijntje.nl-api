dc58306a6f989864f34ce3e77edc2794f3d83215
Martijn <oldmartijntje@gmail.com>
2024-08-30 11:28:58 +0200

    being able to delete a project

--- routing/json.routes.js
+++ routing/json.routes.js
@@ -2,16 +2,15 @@ const express = require("express");
 const { SessionHandler } = require("../authentication/sessionHandler");
 const { projects } = require("../database");
 const settings = require('../settings.json');
+const { UserAuthenticator } = require("../authentication/user.authenticator");
 
 const jsonRouter = express.Router();
 jsonRouter.use(express.json());
 
 /**
- * check if your sessiontoken is valid.
- * 
- * Also returns the tenants that the user is part of.
+ * Get all projects.
  */
-jsonRouter.post("/projects", async (_req, res) => {
+jsonRouter.post("/getProjects", async (_req, res) => {
     try {
         const amount = _req.body.amount ? _req.body.amount : settings.defaultAmountToGet;
         const from = _req.body.from ? _req.body.from : 0;
@@ -41,6 +40,38 @@ jsonRouter.post("/projects", async (_req, res) => {
     } catch (error) {
         res.status(500).send(error.message);
     }
+});
+
+/**
+ * Delete a project.
+ */
+jsonRouter.delete("/projects", async (req, res) => {
+    const { sessionToken, id } = req.query;
+    const sessionH = new SessionHandler();
+    sessionH.rateLimitMiddleware(req, res, async () => {
+        if (!sessionToken || !id) {
+            return res.status(400).send({ error: "Missing sessionToken or id" });
+        }
+        const auth = new UserAuthenticator();
+        const authenticationSuccess = await auth.authenticateBySessionTokenWithResponseHandling(sessionToken, false, res);
+        if (!authenticationSuccess) {
+            return;
+        }
+        if (!auth.checkAuthorityLevel(5)) {
+            res.status(403).send({ "message": "You do not have the required clearance level for this action." });
+            return;
+        }
+        projects.deleteOne({ _id: id }).then((result) => {
+            if (!result) {
+                return res.status(404).send({ message: "Project not found" });
+            }
+            return res.status(200).send({ message: "Project deleted" });
+        }).catch((error) => {
+            return res.status(500).send(error.message);
+
+
+        });
+    });
 });
 
 module.exports = {
