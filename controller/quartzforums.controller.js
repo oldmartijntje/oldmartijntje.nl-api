51469a1dd5202df25f3604e3a7196ee13d6dedd3
oldma <oldmartijntje@gmail.com>
2025-12-04 19:26:08 +0100

    password length

--- controller/quartzforums.controller.js
+++ controller/quartzforums.controller.js
@@ -111,6 +111,11 @@ async function createAccount(req, res) {
             return res.status(400).json({ message: 'Username and password are required' });
         }
 
+        // Validate password length
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+        }
+
         // Sanitize username input
         username = sanitizeInput(username);
 
